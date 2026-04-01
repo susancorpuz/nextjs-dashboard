f71baefe9389b3e0e74024229e523ad80edba313
susancorpuz <susan.corpuz@gmail.com>
2026-04-02 03:42:48 +0800

    Updated pages
    Signed-off-by: susancorpuz <susan.corpuz@gmail.com>

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,6 +4,7 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
+// ...
 export default async function InvoicesTable({
   query,
   currentPage,
@@ -12,6 +13,7 @@ export default async function InvoicesTable({
   currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
+  // ...
 
   return (
     <div className="mt-6 flow-root">
